1671bf85ccb3e6d68fe2259750687a57809035ca
Inderpreet Singh <44306698+Singh1309@users.noreply.github.com>
2022-12-11 18:21:26 -0800

    Update xrdpinstallation.py

--- xrdpinstallation.py
+++ xrdpinstallation.py
@@ -1,1 +1,24 @@
-#installation code here
+'''
+To install xrdp on Ubuntu, you will need to have administrator privileges on the computer and have access to a terminal window. 
+You can follow these steps to install xrdp on Ubuntu:
+Open a terminal window on your Ubuntu computer.
+'''
+
+#Update the package index by running the following command:
+sudo apt update
+#Install xrdp and its dependencies by running the following command:
+sudo apt install xrdp
+#Once the installation is complete, start the xrdp service by running the following command:
+sudo service xrdp start
+#To enable xrdp to start automatically when the computer is booted, run the following command:
+sudo systemctl enable xrdp
+#To verify that xrdp is running and listening on the default port (3389), run the following command:
+sudo netstat -antp | grep xrdp
+
+'''
+If xrdp is running and listening on the default port, you should see output similar to this:
+tcp        0      0 0.0.0.0:3389            0.0.0.0:*               LISTEN      1234/xrdp
+
+If xrdp is running and listening on the default port, you can now connect to the Ubuntu computer using an RDP client, such as the built-in Remote Desktop Connection tool on Windows or the Remote Desktop App on Mac.
+If you encounter any errors or problems during the installation process, you can refer to the xrdp documentation or seek help from the xrdp community.
+'''
